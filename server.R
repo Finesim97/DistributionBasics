b8c526eb757b39980e5d10afd75f70ae15d9e45f
Finesim97 <lukas.jansen1997@yahoo.de>
2020-11-30 21:33:47 +0100

    First version

---
+++ server.R
@@ -0,0 +1,76 @@
+#
+# This is the server logic of a Shiny web application. You can run the
+# application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(ggplot2)
+types = list("Normal" = 1, "LogNormal" = 2,"Binomial"=3,"Hypergeom."=4,
+             "Eruptions" = 5)
+theme_set(theme_linedraw())
+
+# Define server logic required to draw a histogram
+shinyServer(function(input, output) {
+
+    datavals <- reactive({
+        set.seed(input$seed)
+        samples <- input$samples
+        if(input$disttype==types["Eruptions"]){
+            return(sample(faithful$eruptions,size = samples,replace = TRUE))
+        }else if(input$disttype %in% c(types["Normal"],types["LogNormal"])){
+            mean <- input$mean
+            sd <- sqrt(input$var)
+            if (input$disttype == types["Normal"]) {
+                return(rnorm(samples,mean,sd))
+            }else{
+                return(rlnorm(samples,mean,sd))
+            }
+            return(fun(samples,mean,sd))
+        }else if(input$disttype %in% c(types["Binomial"],types["Hypergeom."])){
+            black <- input$hits
+            white <- input$misses
+            draws <- input$draws
+            if (input$disttype == types["Binomial"]) {
+                return(rbinom(samples, draws, prob = black/(white+black)))
+            }else{
+                return(rhyper(samples,white,black,draws))
+            }
+        }
+    })
+    
+    output$boxplots <- renderPlot({
+        quantiletypes <- 1:9
+        df <- cbind(type=as.character(quantiletypes),do.call(rbind,lapply(quantiletypes, function(type)as.data.frame(t(quantile(datavals(),type=type))))))
+        colnames(df) <- c("type","min","low","mid","top","max")
+        ggplot(df, aes(x=type, ymin = min, lower = low, middle = mid, upper = top, ymax = max, group=type)) +
+            geom_boxplot(stat = "identity") + labs(title="Quantile Method Comparisons") + xlab("Method") + ylab("Value")
+    })
+    
+    output$hist <- renderPlot({
+        dat <- data.frame(Value=datavals())
+        ggplot(dat)+ aes(x=Value) +
+            geom_histogram(bins=nclass.Sturges(dat$Value),color="black",fill="white") + labs(title="Histogram", subtitle = "Number of bins according to Sturges") + xlab("Value") + ylab("Count")
+    })
+    output$combi <- renderPlot({
+        dat <- data.frame(Value=datavals())
+        plotbase <- ggplot(dat) + aes(Value) 
+        cowplot::plot_grid(plotbase + geom_boxplot() + ylab ("Boxplot") +
+                               theme(axis.text.x = element_blank(),
+                                     axis.ticks.x = element_blank(),
+                                     axis.title.x = element_blank()), 
+                           plotbase + geom_density() + ylab("Density") +
+                               theme(axis.text.x = element_blank(),
+                                     axis.ticks.x = element_blank(),
+                                     axis.title.x = element_blank()), 
+                           plotbase + stat_ecdf(geom = "step") + xlab("Value") + ylab("Cumulative Frequency"),
+                           ncol = 1,
+                           align = "h",
+                           axis = "bt")
+        
+    })
+
+})
---
+++ ui.R
@@ -0,0 +1,55 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+
+types = list("Normal" = 1, "LogNormal" = 2,"Binomial"=3,"Hypergeom."=4,
+     "Eruptions" = 5)
+
+# Define UI for application that draws a histogram
+shinyUI(fluidPage(
+
+    # Application title
+    titlePanel("Boxplots and Density Estimation" ),
+
+    # Sidebar with a slider input for number of bins
+    sidebarLayout(position = "left",
+        sidebarPanel(
+            h2("Settings"),
+            radioButtons("disttype", h3("Distribution"),
+                         choices = types,selected = 1),
+            helpText("Please select a distribution."),
+            numericInput("seed",h3("Seed"),value = 42),
+            helpText("Seed for sample generation"),
+            numericInput("samples",h4("Samples"),value = 10, min = 1, max = 10**12),
+            helpText("Number of samples to draw"),
+            h3("Normal Dist. Parameters"),
+            numericInput("mean",h4("Mean"),value = 0),
+            helpText("Mean of the distribution"),
+            numericInput("var",h4("Variance"),value = 1, min=0),
+            helpText("Variance of the distribution"),
+            h3("Discrete Distribution Parameters"),
+            numericInput("draws",h4("Drawn balls"),value = 10, min=1),
+            helpText("Number of balls to draw from the urn"),
+            numericInput("hits",h4("Hit balls"),value = 10, min=1),
+            helpText("Number of 'hit' balls in the urn"),
+            numericInput("misses",h4("Miss balls"),value = 90, min=1),
+            helpText("Number of 'miss' balls in the urn")
+        ),
+
+        # Show a plot of the generated distribution
+        mainPanel(
+            tabsetPanel(type = "tabs",
+                        tabPanel("Boxplot Methods", plotOutput("boxplots",height = "600px")),
+                        tabPanel("Histogram", plotOutput("hist", height = "600px")),
+                        tabPanel("Comparisons", plotOutput("combi", height = "750px"))
+            )
+        )
+    )
+))
