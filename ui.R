11bac5049eee2cde2f7b59b6b30942f86b389b69
Lukas Jansen <lukas.jansen1997@yahoo.de>
2020-12-02 20:31:22 +0100

    Update ui.R

--- ui.R
+++ ui.R
@@ -12,23 +12,6 @@ library(shiny)
 types = list("Normal" = 1, "LogNormal" = 2,"Binomial"=3,"Hypergeom."=4,
      "Eruptions" = 5)
 
-# https://stackoverflow.com/a/9195691
-extract_help <- function(pkg, fn = NULL, to = c("txt", "html", "latex", "ex")){
-  to <- match.arg(to)
-  rdbfile <- file.path(find.package(pkg), "help", pkg)
-  rdb <- tools:::fetchRdDB(rdbfile, key = fn)
-  convertor <- switch(to, 
-                      txt   = tools::Rd2txt, 
-                      html  = tools::Rd2HTML, 
-                      latex = tools::Rd2latex, 
-                      ex    = tools::Rd2ex
-  )
-  f <- function(x) capture.output(convertor(x))
-  if(is.null(fn)) lapply(rdb, f) else f(rdb)
-}
-helptext <- extract_help("stats","quantile","txt")
-pos <- sapply(c("_\\\bT_\\\by_\\\bp_\\\be_\\\bs:","_\\\bA_\\\bu_\\\bt_\\\bh_\\\bo_\\\br\\(_\\\bs\\):"),grep,helptext)
-helptext <- paste0(helptext[(pos[1]+1):(pos[2]-1)],collapse = "\n")
 
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
@@ -64,7 +47,7 @@ shinyUI(fluidPage(
         # Show a plot of the generated distribution
         mainPanel(
             tabsetPanel(type = "tabs",
-                        tabPanel("Boxplot Methods", plotOutput("boxplots",height = "600px"), renderText({helptext})),
+                        tabPanel("Boxplot Methods", plotOutput("boxplots",height = "600px"), textOutput("helptext")),
                         tabPanel("Histogram", plotOutput("hist", height = "600px")),
                         tabPanel("Comparisons", plotOutput("combi", height = "750px"))
             )
